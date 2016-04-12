41c70971b8d94abfdd9d30eae3945a143d230aab
Yun Wu <yun.wu@qub.ac.uk>
2016-04-12 20:20:31 +0100

    initial-commit-20160412

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 35 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#35 SMP PREEMPT Fri Feb 27 10:27:15 GMT 2015"
+#define LINUX_COMPILE_BY "jackywu"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.7.3 (Ubuntu/Linaro 4.7.3-12ubuntu1) "
